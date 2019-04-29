bc24e74cab00313c240cef90cdc90280c42e890a
Dong <black_dragon@foxmail.com>
2019-04-29 19:09:26 +0800

    使用redis对category分类查询进行优化

--- src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
+++ src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -4,8 +4,12 @@ import cn.itcast.travel.dao.CategoryDao;
 import cn.itcast.travel.dao.impl.CategoryDaoImpl;
 import cn.itcast.travel.domain.Category;
 import cn.itcast.travel.service.CategoryService;
+import cn.itcast.travel.util.JedisUtil;
+import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class CategoryServiceImpl implements CategoryService {
 
@@ -17,6 +21,31 @@ public class CategoryServiceImpl implements CategoryService {
      */
     @Override
     public List<Category> findAll() {
-        return categoryDao.findAll();
+
+        //1.先从redis中查询
+        Jedis jedis = JedisUtil.getJedis();
+        //1.1 期望获取的数据有顺序
+        Set<String> categorys = jedis.zrange("category", 0, -1);
+
+        //2.判断查询的集合是否为null
+        List<Category> cs = null;
+        if (categorys == null || categorys.size() == 0){
+            //如果为空:第一次访问,需要查询数据库,将数据存入redis
+            cs = categoryDao.findAll();
+            //期望获取的数据有顺序
+            for (int i = 0; i < cs.size(); i++) {
+                jedis.zadd("category",cs.get(i).getCid(),cs.get(i).getCname());
+            }
+
+        }else {
+            //如果不为空:不是第一次访问,将Set中的数据存入List
+            cs = new ArrayList<Category>();
+            for (String c:categorys) {
+                Category category = new Category();
+                category.setCname(c);
+                cs.add(category);
+            }
+        }
+        return cs;
     }
 }
